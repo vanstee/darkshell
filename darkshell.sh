344e3b3fe01f5a90fb33c253c1d38861f00401fd
vanstee <vanstee@highgroove.com>
2013-04-15 20:08:12 -0400

    Echo an icon for the forecast code

--- darkshell.sh
+++ darkshell.sh
@@ -3,10 +3,46 @@
 oauth_token=$1
 forcast_key=$2
 
-location=$(curl --header $oauth_token https://www.googleapis.com/latitude/v1/currentLocation?granularity=best)
+location=$(curl --header "$oauth_token" https://www.googleapis.com/latitude/v1/currentLocation?granularity=best)
 latitude=$(echo $location | cut -d ',' -f 3 | cut -d ':' -f 2)
 longitude=$(echo $location | cut -d ',' -f 4 | cut -d ':' -f 2)
 
 current_time=$(date +"%s")
-forcast=$(curl https://api.forecast.io/forecast/$forcast_key/$latitude,$longitude,$current_time?exclude=minutely,hourly,daily,alerts,flags)
+forecast=$(curl https://api.forecast.io/forecast/$forcast_key/$latitude,$longitude,$current_time?exclude=minutely,hourly,daily,alerts,flags)
 icon=$(echo $forecast | cut -d ',' -f 7 | cut -d ':' -f 2 | sed s/\"//g)
+
+case "$icon" in
+  "clear-day")
+    echo "☼"
+    ;;
+  "clear-night")
+    echo "☾"
+    ;;
+  "thunderstorm")
+    echo "⚡"
+    ;;
+  "rain")
+    echo "☔"
+    ;;
+  "snow")
+    echo "❆"
+    ;;
+  "sleet")
+    echo "❄"
+    ;;
+  "wind")
+    echo "⚐"
+    ;;
+  "fog")
+    echo "☁"
+    ;;
+  "cloudy")
+    echo "☁"
+    ;;
+  "partly-cloudy-day")
+    echo "☼"
+    ;;
+  "partly-cloudy-night")
+    echo "☆"
+    ;;
+esac
