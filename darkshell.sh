fa55b8d5980a6ed59ae91b2993999264c075e3ff
vanstee <vanstee@highgroove.com>
2013-04-15 20:13:29 -0400

    Use environment variables :zap:

--- darkshell.sh
+++ darkshell.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-oauth_token=$1
-forcast_key=$2
+if ! [ -n "${GOOGLE_LATITUDE_OAUTH_TOKEN}" ]; then
+  echo "Please set the GOOGLE_LATITUDE_OAUTH_TOKEN environment vairable"
+  exit 1
+fi
 
-location=$(curl --header "$oauth_token" https://www.googleapis.com/latitude/v1/currentLocation?granularity=best)
+if ! [ -n "${FORECAST_IO_KEY}" ]; then
+  echo "Please set the FORECAST_IO_KEY environment vairable"
+  exit 1
+fi
+
+location=$(curl --header "$GOOGLE_LATITUDE_OAUTH_TOKEN" https://www.googleapis.com/latitude/v1/currentLocation?granularity=best)
 latitude=$(echo $location | cut -d ',' -f 3 | cut -d ':' -f 2)
 longitude=$(echo $location | cut -d ',' -f 4 | cut -d ':' -f 2)
 
 current_time=$(date +"%s")
-forecast=$(curl https://api.forecast.io/forecast/$forcast_key/$latitude,$longitude,$current_time?exclude=minutely,hourly,daily,alerts,flags)
+forecast=$(curl https://api.forecast.io/forecast/$FORECAST_IO_KEY/$latitude,$longitude,$current_time?exclude=minutely,hourly,daily,alerts,flags)
 icon=$(echo $forecast | cut -d ',' -f 7 | cut -d ':' -f 2 | sed s/\"//g)
 
 case "$icon" in
